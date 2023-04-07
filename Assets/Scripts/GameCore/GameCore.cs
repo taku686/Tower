36a253b7f57adfa1a9da4d7a57d5882d7f29c5fc
DESKTOP <takunoshin123456789@gmail.com>
2023-04-07 15:04:17 +0900

    ログイン処理を作成。

--- Assets/Scripts/GameCore/GameCore.cs
+++ Assets/Scripts/GameCore/GameCore.cs
@@ -1,13 +1,25 @@
 using System;
+using System.Collections.Generic;
+using DefaultNamespace;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public partial class GameCore : MonoBehaviour
 {
     private StateMachine<GameCore> _stateMachine;
 
+    [SerializeField] private PlayFabLoginManager playFabLoginManager;
+    [SerializeField] private TitleView titleView;
+    [SerializeField] private BattleModeSelectView battleModeSelectView;
+    [SerializeField] private BattleReadyView battleReadyView;
+    [SerializeField] private BattleView battleView;
+    [SerializeField] private BattleResultView battleResultView;
+    [SerializeField] private List<GameObject> uiObjects = new();
+
     private enum Event
     {
         Title,
+        BattleModeSelect,
         BattleReady,
         Battle,
         BattleResult
@@ -20,12 +32,22 @@ public partial class GameCore : MonoBehaviour
 
     private void InitializeState()
     {
+        _stateMachine = new StateMachine<GameCore>(this);
         _stateMachine.Start<TitleState>();
-        _stateMachine.AddTransition<TitleState, BattleReadyState>((int)Event.BattleReady);
+        _stateMachine.AddTransition<TitleState, BattleModeSelectState>((int)Event.BattleModeSelect);
+        _stateMachine.AddTransition<BattleModeSelectState, BattleReadyState>((int)Event.BattleReady);
         _stateMachine.AddTransition<BattleReadyState, BattleState>((int)Event.Battle);
         _stateMachine.AddTransition<BattleState, BattleReadyState>((int)Event.BattleResult);
         _stateMachine.AddTransition<BattleResultState, TitleState>((int)Event.Title);
     }
-    
-    
+
+    private void SwitchUiView(int index)
+    {
+        foreach (var obj in uiObjects)
+        {
+            obj.SetActive(false);
+        }
+
+        uiObjects[index].SetActive(true);
+    }
 }
---
+++ Assets/Scripts/PlayFab/PlayFabLoginManager.cs
@@ -0,0 +1,48 @@
+﻿using Cysharp.Threading.Tasks;
+using Data;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PlayFabLoginManager : MonoBehaviour
+    {
+        private GetPlayerCombinedInfoRequestParams _info;
+
+
+        public void Initialize()
+        {
+            PlayFabSettings.staticSettings.TitleId = GameCommonData.TitleId;
+        }
+
+        public async UniTask<bool> Login()
+        {
+            _info = new GetPlayerCombinedInfoRequestParams()
+            {
+                GetUserData = true,
+                GetUserAccountInfo = true,
+                GetTitleData = true,
+                GetUserVirtualCurrency = true,
+                GetUserInventory = true,
+                GetPlayerProfile = true
+            };
+            var request = new LoginWithAndroidDeviceIDRequest()
+            {
+                CreateAccount = true,
+                InfoRequestParameters = _info,
+                AndroidDeviceId = SystemInfo.deviceUniqueIdentifier,
+            };
+            var response = await PlayFabClientAPI.LoginWithAndroidDeviceIDAsync(request);
+
+            if (response.Error != null)
+            {
+                Debug.LogError(response.Error.GenerateErrorReport());
+                return false;
+            }
+
+            Debug.Log("login");
+            return true;
+        }
+    }
+}
